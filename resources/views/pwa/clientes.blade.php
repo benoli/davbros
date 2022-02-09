8ab77bcc2dea69912e0186647784cc5aa8950372
Andres Gil <andres_gil@me.com>
2022-02-09 17:04:35 -0300

    Clientes done. Alpha version.

--- resources/views/pwa/clientes.blade.php
+++ resources/views/pwa/clientes.blade.php
@@ -37,64 +37,22 @@
         <!-- add recipe side nav -->
     <div id="side-form" class="sidenav side-form">
       <form id="form-add-cliente" class="add-recipe container section">
-        <h6 >Nuevo Cliente <i class="right material-icons">person_add</i></h6>
+        <h6 >Nuevo Cliente <i class="right material-icons">add</i></h6>
         <div class="divider"></div>
         <div class="input-field">
           <input placeholder="Nombre" id="name" type="text" class="validate" name="name" required>
           <label for="name">Nombre</label>
         </div>
         <div class="input-field">
-          <input placeholder="Apellido" id="lastname" type="text" class="validate" name="lastname" required>
-          <label for="lastname">Apellido</label>
-        </div>
-        <div class="input-field">
-          <input placeholder="DNI" id="dni" type="text" class="validate" name="dni" required>
-          <label for="dni">DNI</label>
-        </div>
-        <div class="input-field">
-          <input placeholder="F. Nac." id="fecha_nac" type="date" class="validate" name="fecha_nac" required>
-          <label for="fecha_nac">Fecha Nacimiento</label>
-        </div>
-        <div class="input-field">
-          <input placeholder="Sexo" id="sex" type="text" class="validate" name="sex" required>
-          <label for="sex">Sexo</label>
-        </div>
-        <div class="input-field">
-          <input placeholder="Email" id="email" type="text" class="validate" name="email" required>
+          <input placeholder="Email" id="email" type="text" class="validate" name="email">
           <label for="email">Email</label>
         </div>
         <div class="input-field">
-          <input placeholder="Dirección" id="address" type="text" class="validate" name="address">
-          <label for="address">Dirección</label>
-        </div>
-        <div class="input-field">
-          <input placeholder="Celular" id="phone" type="text" class="validate" name="phone" required>
-          <label for="phone">Celular</label>
-        </div>
-        <div class="input-field">
-          <input placeholder="Celular Alternativo" id="alternative_phone" type="text" class="validate" name="alternative_phone" required>
-          <label for="alternative_phone">Celular Alternativo</label>
-        </div>
-        <div class="input-field">
-          <select id="select-tipo" name="tipo" class="validate"></select>
-          <label for="tipo">Tipo de cliente</label>
-        </div>
-        <div>
-            <video id="video" class="scanner-window" width="300" height="200" style="border: 1px solid gray; display:none"></video>
-        </div>
-        <div id="sourceSelectPanel" style="display:none">
-          <label for="sourceSelect">Elegir cámara</label>
-          <select id="sourceSelect" style="max-width:200px">
-          </select>
+          <input placeholder="Celular" id="phone" type="text" class="validate" name="phone">
+          <label for="phone">Teléfono</label>
         </div>
         <div class="input-field center">
-          <button class="btn-small" id="scanner">DNI con Lector</button>
-        </div>
-        <div class="input-field center">
-          <button class="btn-small" id="camera">DNI con cámara</button>
-        </div>
-        <div class="input-field center">
-          <button class="btn-small" id="add-user">Agregar</button>
+          <button class="btn-small" id="add-client">Agregar</button>
         </div>
       </form>
     </div>
